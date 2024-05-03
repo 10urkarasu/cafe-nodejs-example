a0ab98cf02f17a49f82dd2e29bacda07a555ce5a
10urkarasu <10urkarasu@gmail.com>
2024-05-03 17:45:04 +0300

    added server and routes

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 const http = require("http");
+const url = require("url");
 
-const hostname = "127.0.0.1";
-const port = 3000;
+const host = "127.0.0.1";
+const port = 8000;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end("Hello, World!\n");
+    const { query, pathname } = url.parse(req.url, true);
+    if (pathname === "/") {
+        res.writeHead(200, {
+            "Content-type": "text/html",
+        });
+        console.log("Anasayfa");
+        res.end();
+    } else if (pathname === "/menu") {
+        res.writeHead(200, {
+            "Content-type": "text/html",
+        });
+        console.log("Menü");
+        res.end();
+    } else if (pathname === "/contact") {
+        res.writeHead(200, {
+            "Content-type": "text/html",
+        });
+        console.log("İletişim");
+        res.end();
+    } else {
+        res.writeHead(404, {
+            "Content-type": "text/html",
+        });
+        res.end("<h1>Page not found!</h1>");
+    }
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, host, () => {
+    console.log(`Listening to requests from ${host} on port ${port}`);
 });
